99a7bdf2f7273956b1926198531904997868c265
TommasoMenghini <tommasomenghini@gmail.com>
2026-02-02 16:48:29 +0000

    Deploying to gh-pages from @ TommasoMenghini/TommasoMenghini.github.io@8a3f12e1d4b714d0728292746f70c693ff8bb851 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -426,9 +426,9 @@ ninja.data = [{
           description: "Generalized Additive Models per la Previsione dei Prezzi delle Case a Milano",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{
